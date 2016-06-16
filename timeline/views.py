46721674ca67f341e8e460ec58665dfc0b24aca1
eunjungchoi <im.your.energy@gmail.com>
2016-06-17 00:19:55 +0900

    code refactoring
    카테고리 배열 생성

--- timeline/views.py
+++ timeline/views.py
@@ -7,6 +7,14 @@ from timeline.models import *
 
 def index(request):
 	study_list = Study.objects.all().order_by('date')
+	for study in study_list:
+		study.cats = []
+		study.cats.append(study.cat01)
+		study.cats.append(study.cat02)
+		study.cats.append(study.cat03)
+		study.cats.append(study.cat04)
+		study.cats.append(study.cat05)
+
 	context = {
 		'study_list' : study_list,
 	}
