9e6cf10086c9e2f262502cb2f6cfeef014dff6c8
eunjungchoi <im.your.energy@gmail.com>
2016-06-20 22:10:15 +0900

    add input form

--- timeline/models.py
+++ timeline/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Study(models.Model):
 	title = models.CharField(max_length=200)
 	date = models.DateField()
 	category = models.ManyToManyField('Category') 
-
 
 	def __str__(self):
 		return self.title
@@ -18,25 +15,3 @@ class Category(models.Model):
 	
 	def __str__(self):
 			return self.name
-
-
-
-# cat02 = models.BooleanField()
-	# cat03 = models.BooleanField()
-	# cat04 = models.BooleanField()
-	# cat05 = models.BooleanField()
-	# cat06 = models.BooleanField()
-	# cat07 = models.BooleanField()
-	# cat08 = models.BooleanField()
-	# cat09 = models.BooleanField()
-	# cat10 = models.BooleanField()
-
-	# name02 = models.CharField(max_length=30)
-	# name03 = models.CharField(max_length=30)
-	# name04 = models.CharField(max_length=30)
-	# name05 = models.CharField(max_length=30)
-	# name06 = models.CharField(max_length=30)
-	# name07 = models.CharField(max_length=30)
-	# name08 = models.CharField(max_length=30)
-	# name09 = models.CharField(max_length=30)
-	# name10 = models.CharField(max_length=30)
--- timeline/views.py
+++ timeline/views.py
@@ -8,11 +8,13 @@ from timeline.models import *
 
 def index(request):
 	study_list = Study.objects.all().order_by('date')
+	for study in study_list:
+		study.cat = [ each_cat.name for each_cat in study.category.all()]
 	categories = Category.objects.all()
 
 	context = {
 		'study_list' : study_list,
-		'categories' : categories
+		'categories' : categories,
 	}
 
 	return render(request, 'timeline/index.html', context)
@@ -29,41 +31,19 @@ def form(request):
 
 
 def add(request):
+	categories = Category.objects.all()
+
 	s = Study(
 		title=request.POST['title'],
-		date=request.POST['date'], 
-		category=request.POST.getlist('category')
+		date=request.POST['date']
 		)
 	s.save()
 
+
+	cat_list = request.POST.getlist('category')
+
+	for cat in cat_list:
+		each_cat = Category.objects.get(name=cat)
+		s.category.add(each_cat)
+
 	return redirect('/')
-	
-
-
-
-	# study_list = Study.objects.all().order_by('date')
-	# categories = Category.objects.all()
-
-	# context = {
-	# 		'study_list' : study_list,
-	# 	}
-
-	# return render(request, 'timeline/index.html', context)
-
-	# for study in study_list:
-	# 	study.cats = []
-	# 	study.cats.append(study.cat01)
-	# 	study.cats.append(study.cat02)
-	# 	study.cats.append(study.cat03)
-	# 	study.cats.append(study.cat04)
-	# 	study.cats.append(study.cat05)
-
-		# cat02=request.POST['cat02'],
-		# cat03=request.POST['cat03'],
-		# cat04=request.POST['cat04'],
-		# cat05=request.POST['cat05'],
-		# cat06=request.POST['cat06'],
-		# cat07=request.POST['cat07'],
-		# cat08=request.POST['cat08'],
-		# cat09=request.POST['cat09'],
-		# cat10=request.POST['cat10']
