77c78de48a060c7298cc98ff0fcb14b01ae49675
eunjungchoi <im.your.energy@gmail.com>
2016-07-05 03:06:52 +0900

    #43 ADD Facebook profile image

--- timeline/views/home.py
+++ timeline/views/home.py
@@ -7,6 +7,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.db.models import Q
 from timeline.models import *
+import requests
+
 
 
 @login_required
@@ -16,10 +18,21 @@ def index(request):
 		study.cat = [ each_cat.name for each_cat in study.category.all()]
 	
 	user = request.user
+	social = user.social_auth.get()
+	url = 'https://graph.facebook.com/{0}/picture?type=small'.format(social.uid)
+
+	# 오래 걸림 
+	# response = requests.request('GET', url, 
+	# 	params={'redirect':0, 'access_token': social.extra_data['access_token']}
+	# 	)
+	# response.raise_for_status()
+	# profile_picture_url = response.json()['data']['url']
+
 
 	categories = Category.objects.filter(user=request.user)
 
-	try:
+	try: 
+		 
 		timeline = Timeline.objects.get(owner=request.user)
 		follower_IDs = [ each.id for each in timeline.followers.all()]
 	except:
@@ -31,6 +44,7 @@ def index(request):
 		'categories' : categories,
 		'user' : user,
 		'follower_IDs' : follower_IDs,
+		'profile_picture_url' : url
 		}
 	return render(request, 'timeline/index.html', context)
 
