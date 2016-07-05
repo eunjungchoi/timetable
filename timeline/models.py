c0e5a1ffff3110c7040b9f8fe01516271abfdf0f
eunjungchoi <im.your.energy@gmail.com>
2016-07-05 15:05:38 +0900

    #50 ADD created, updated on Study

--- timeline/models.py
+++ timeline/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.utils import timezone
+
 
 class Study(models.Model):
+	title = models.CharField(max_length=100)
+	contents = models.TextField(max_length=500)
+	date = models.DateField()
+
 	user = models.ForeignKey(User)
 	timeline = models.ForeignKey('Timeline', null=True, blank=True, default = None)
-	title = models.CharField(max_length=100)
-	date = models.DateField()
-	contents = models.TextField(max_length=500)
 	category = models.ManyToManyField('Category')
+
+	created_at = models.DateTimeField(null=True, default=None)
+	updated_at = models.DateTimeField(null=True, default=None)
+
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.created_at = timezone.now()
+		self.updated_at = timezone.now()
+
+		return super(Study, self).save(*args, **kwargs)
 
 
 	def __str__(self):
@@ -23,9 +37,10 @@ class Study(models.Model):
 
 
 class Category(models.Model):
+	name = models.CharField(max_length=30)
+
 	user = models.ForeignKey(User)
 	timeline = models.ForeignKey('Timeline', null=True, blank=True, default = None)
-	name = models.CharField(max_length=30)
 	
 	def __str__(self):
 			return self.name
@@ -39,8 +54,14 @@ class Timeline(models.Model):
 		return self.owner.username
 
 
+def create_user_default_timeline(sender, instance, created, **kwargs):
+    if created:
+        Timeline.objects.create(owner=instance)
+
+
+
 #################
-# 풀어서 쓰면, 이런 느낌.
+# class Timeline >> 풀어서 쓰면,
 # 
 # class Audience(models.Model):
 # 	user = models.ForeignKey(User, related_name='owner')
@@ -55,8 +76,6 @@ class Timeline(models.Model):
 # 	user = models.ForeignKey(User)
 # 	viewer = models.ForeignKey(User)
 
-def create_user_default_timeline(sender, instance, created, **kwargs):
-    if created:
-        Timeline.objects.create(owner=instance)
+
 
 post_save.connect(create_user_default_timeline, sender=User)
