77b8e3eb48a04510d33943065309bf14fd735f12
eunjungchoi <im.your.energy@gmail.com>
2016-07-05 19:01:10 +0900

    #53 REFACTOR views.follow

--- timeline/views/follow.py
+++ timeline/views/follow.py
@@ -41,29 +41,20 @@ def account(request, user_id):
 def add_follower(request):
 	try:
 		timeline = Timeline.objects.get(owner=request.user)
-	except:
-		timeline = Timeline(
-			owner=request.user
-			)
-		timeline.save
-
+	except Timeline.DoesNotExist:
+		return render(request, '404.html')
+		
 	user = request.user
 	follower_id = int(request.POST['follower_id_to_add'])
 
-
 	try:
 		follower = User.objects.get(pk=follower_id)
-		if follower is request.user:
-			follower = None
-
+		if follower_id == request.user.id:
+			pass
 		elif not user.followings.filter(id=follower_id).exists():
-			# follower = User.objects.get(pk=follower_id)
 			timeline.followers.add(follower)
 	except: 
-		follower = None
-
-	# if not user.followings.filter(id=follower_id).exists():
-	# 	timeline.followers.add(follower)
+		pass
 
 	return redirect(reverse('index'))
 
@@ -72,18 +63,16 @@ def add_follower(request):
 def delete_follower(request):
 	follower_id_list_to_del = request.POST.getlist('each_id_to_del')
 	user = request.user
+	timeline = Timeline.objects.get(owner=request.user)
 
 	for each_id in follower_id_list_to_del:
 		follower = User.objects.get(pk=int(each_id))
-		timeline = Timeline.objects.get(owner=request.user)
 		timeline.followers.remove(follower)
-		# timeline.followers = timeline.followers.exclude(id=follower.id)
+		# timeline.followers = timeline.followers.exclude(id=follower.id)  << 이렇게 해도 동일한 결과
 
-
-		# user.followings.filter(followers=follower).delete()
-		# timeline.followers.get(user_id=each_id).delete()
 		# each_follower = timeline.followers.get(id=int(each_id))
-		# each_follower.delete()  <= 이렇게 하면 user 인스턴스가 통째로 삭제됨. 본의 아니게 남의 계정을 삭제하는 부작용
+		# each_follower.delete()  
+		# : 이렇게 하면 user 인스턴스가 통째로 삭제됨. 본의 아니게 남의 계정을 삭제하는 부작용
 	
 	return redirect(reverse('index'))
 
