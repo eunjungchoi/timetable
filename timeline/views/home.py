70dac90657a0c55b72947d369727be2b4a887377
eunjungchoi <im.your.energy@gmail.com>
2016-07-08 00:28:22 +0900

    ADD viewer_mode on detail

--- timeline/views/home.py
+++ timeline/views/home.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
 from timeline.models import *
 
 
@@ -10,7 +12,6 @@ def index(request):
 		# study.cat = [ each_cat.name for each_cat in study.category.all()]
 		study.cat = study.get_category_names()
 	
-	user = request.user
 	try:
 		social = user.social_auth.get()
 		url = 'https://graph.facebook.com/{0}/picture?type=small'.format(social.uid)
@@ -28,7 +29,6 @@ def index(request):
 	context = {
 		'study_list' : study_list,
 		'categories' : categories,
-		'user' : user,
 		'viewer_IDs' : viewer_IDs,
 		'profile_picture_url' : url
 		}
@@ -43,16 +43,37 @@ def index(request):
 	# profile_picture_url = response.json()['data']['url']
 
 
+
 @login_required
 def detail(request, study_id):
-	categories = Category.objects.filter(user=request.user)
-	study = Study.objects.filter(user=request.user).get(pk=study_id)
-	# study.cat = [ each_cat.name for each_cat in study.category.all()]
-	study.cat = study.get_category_names()
+	study = get_object_or_404(Study, pk=study_id)
+	owner = get_object_or_404(User, pk=study.user.id)
+	timeline = get_object_or_404(Timeline, owner=owner)
 
-	
+	same_user = request.user.pk == owner.pk
+	has_permission = timeline.viewers.filter(id=request.user.id).exists()
+
+	if (not same_user) and (not has_permission):
+		return render(request, '403.html')
+
+	study.cat = study.get_category_names()
 	context = {
 		'study' : study,
-		'categories': categories
+		'owner' : owner,
+		'same_user' : same_user,
 	}
 	return render(request, 'timeline/detail.html', context)
+
+
+
+
+# @login_required
+# def old_detail(request, study_id):
+# 	study = Study.objects.filter(user=request.user).get(pk=study_id)
+# 	study.cat = study.get_category_names()
+	
+# 	context = {
+# 		'study' : study,
+# 		'categories': categories
+# 	}
+# 	return render(request, 'timeline/detail.html', context)
