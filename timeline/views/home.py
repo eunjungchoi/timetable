451511657024ebf7601422c55005f9ab7c204720
eunjungchoi <im.your.energy@gmail.com>
2016-07-19 20:45:53 +0900

    REFACTOR with annotation & Count

--- timeline/views/home.py
+++ timeline/views/home.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-
+from django.db.models import Count
 from timeline.models import *
 
 
@@ -13,11 +13,11 @@ def index(request):
 	# study_list = LatestStudy.objects.filter(user=user)
 	for study in study_list:
 		study.cat = study.get_category_names()
-	
+
 	timeline = user.timeline_set.all()[0]
 	viewer_IDs = [ each.id for each in timeline.viewers.all()]
 
-	
+
 	context = {
 		'study_list' : study_list,
 		'categories' : user.category_set.all(),
@@ -27,7 +27,7 @@ def index(request):
 
 
 # 다른 방법. (오래 걸림)
-	# response = requests.request('GET', url, 
+	# response = requests.request('GET', url,
 	# 	params={'redirect':0, 'access_token': social.extra_data['access_token']}
 	# 	)
 	# response.raise_for_status()
@@ -58,20 +58,16 @@ def detail(request, study_id):
 @login_required
 def cal(request):
 	user = request.user
-	categories = user.category_set.all()
-	for cat in categories:
-		cat.studynum = user.studies.filter(category=cat).count()
+	cats = Category.objects.filter(user=user).annotate(num_study=Count('study'))
+	# 풀어쓰면 아래와 같음
+	# categories = user.category_set.all()
+	# for cat in categories:
+	# 	cat.studynum = user.studies.filter(category=cat).count()
 
-	# study = get_object_or_404(Study, pk=study_id)
-	# study.cat = study.get_category_names()
 	context = {
-		'categories' : categories
+		'categories' : cats
 	}
 	return render(request, 'timeline/cal.html', context)
 
 # 쿼리셋의 각 아이템 별 개수 세기
 # http://stackoverflow.com/questions/5439901/getting-a-count-of-objects-in-a-queryset-in-django
-
-
-
-
