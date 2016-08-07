dc88e0d88b1569c1f194b0dc6b49af9ed765ce6f
Eun Jung Choi <im.your.energy@gmail.com>
2016-08-07 23:33:21 +0900

    REMOVE default explanation

--- timetable/wsgi.py
+++ timetable/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for timetable project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
@@ -16,4 +7,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timetable.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
