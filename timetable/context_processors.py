e23bd28d6922563a02217464d92c1a5b6459722b
Eun Jung Choi <im.your.energy@gmail.com>
2016-10-22 02:32:33 +0900

    ADD comments

--- timetable/context_processors.py
+++ timetable/context_processors.py
@@ -18,3 +18,9 @@ def auth(request):
     return {
         'user': user,
     }
+
+#'django.contrib.auth.context_processors.auth',
+# 여기에서  auth 부분을 가져와서 overwrite 한 것.
+# 그리고 setting 에  추가됨. (템플릿에))
+# 템플릿에 등장한 user 만  user profile image가  가져와짐.
+# request.user 일때는  원래 user로...
